d3d5d93303e3bf0771687999c9bd616d54d37b7f
amitsingh8268 <amitsingh8268@gmail.com>
2024-05-02 21:24:39 +0530

    Delete POCMVC directory

--- POCMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace POCMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
